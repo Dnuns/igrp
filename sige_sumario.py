7e291a2efacec83518cbbab417501a23dc8e0474
David Nunes <davsnuns@gmail.com>
2021-04-15 22:07:09 -0100

    refactor code create login and summary function

--- sige_sumario.py
+++ sige_sumario.py
@@ -18,45 +18,43 @@ from time import sleep
 
 
 # Iª PARTE
-
 browser = webdriver.Firefox()
-browser.get('https://nosiapps.gov.cv/redglobal/redglobal.glb_dispatcher.login')
 
-browser.implicitly_wait(20)
+def login():
 
-for i in range (0,2): # fazer login
-
-    esperar_login = WebDriverWait(browser, 20)
-    utilizador = esperar_login.until(EC.element_to_be_clickable((By.NAME, 'p_login')))
-    browser.find_element_by_name('p_login').send_keys(config.username)
-    Keys.TAB
-    password = esperar_login.until(EC.element_to_be_clickable((By.NAME, 'p_password')))
-    browser.find_element_by_name('p_password').send_keys(config.password)
-    browser.find_element_by_name('p_button').click()
+    browser.get('https://nosiapps.gov.cv/redglobal/redglobal.glb_dispatcher.login')
     browser.implicitly_wait(20)
 
-browser.implicitly_wait(20)
-browser.find_element_by_class_name('app-icon').click()
-
-esperar_menu = WebDriverWait(browser,20)
-menu_lateral = esperar_menu.until(EC.element_to_be_clickable((By.ID,'side-bar-ctrl')))
-browser.find_element_by_id('side-bar-ctrl').click()#abrir o menu
-
-menu_gestao_disciplina = esperar_menu.until(EC.element_to_be_clickable((By.XPATH,'/html/body/form/div[1]/div/div[1]/ul/li[2]/a')))
-browser.find_element_by_xpath('/html/body/form/div[1]/div/div[1]/ul/li[2]/a').click()#Menu gestao de disciplina
-
-browser.find_element_by_xpath('/html/body/form/div[1]/div/div[1]/ul/li[2]/ul/li[2]/a/span').click()# Menu principal do professor
-
-sala_de_aula = esperar_menu.until(EC.element_to_be_clickable((By.XPATH, '/html/body/form/div[1]/div/div[2]/div/div[2]/div/div/div/ul/li[2]/a/span')))
-browser.find_element_by_xpath('/html/body/form/div[1]/div/div[2]/div/div[2]/div/div/div/ul/li[2]/a/span').click()# menu sala de aula
-
-browser.find_element_by_xpath('/html/body/form/div[1]/div/div[2]/div/div[2]/div/div/div/div/div[2]/div/div/div/div/div/a[2]/span').click() #submenu resumo do dia
+    for i in range (0,2): # fazer login
+        wait_login = WebDriverWait(browser, 20)
+        utilizador = wait_login.until(EC.element_to_be_clickable((By.NAME, 'p_login')))
+        browser.find_element_by_name('p_login').send_keys(config.username)
+        Keys.TAB
+        password = wait_login.until(EC.element_to_be_clickable((By.NAME, 'p_password')))
+        browser.find_element_by_name('p_password').send_keys(config.password)
+        browser.find_element_by_name('p_button').click()
+        sleep(2)
 
 
+def summary_page():
+
+    browser.implicitly_wait(20)
+    browser.find_element_by_class_name('app-icon').click()
+    wait_menu = WebDriverWait(browser,20)
+    menu_lateral = wait_menu.until(EC.element_to_be_clickable((By.ID,'side-bar-ctrl')))
+    browser.find_element_by_id('side-bar-ctrl').click()#abrir o menu
+    menu_gestao_disciplina = wait_menu.until(EC.element_to_be_clickable((By.XPATH,'/html/body/form/div[1]/div/div[1]/ul/li[2]/a')))
+    browser.find_element_by_xpath('/html/body/form/div[1]/div/div[1]/ul/li[2]/a').click()#Menu gestao de disciplina
+    browser.find_element_by_xpath('/html/body/form/div[1]/div/div[1]/ul/li[2]/ul/li[2]/a/span').click()# Menu principal do professor
+    sala_de_aula = wait_menu.until(EC.element_to_be_clickable((By.XPATH, '/html/body/form/div[1]/div/div[2]/div/div[2]/div/div/div/ul/li[2]/a/span')))
+    browser.find_element_by_xpath('/html/body/form/div[1]/div/div[2]/div/div[2]/div/div/div/ul/li[2]/a/span').click()# menu sala de aula
+    browser.find_element_by_xpath('/html/body/form/div[1]/div/div[2]/div/div[2]/div/div/div/div/div[2]/div/div/div/div/div/a[2]/span').click() #submenu resumo do dia
 
 
-
-
+#Init the script
+login()
+sleep(3)
+summary_page()
 
 
 
