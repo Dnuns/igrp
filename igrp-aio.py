bb237eca05f27339ff272bb3471ed166e9821234
David Nunes <davsnuns@gmail.com>
2021-04-18 18:32:13 -0100

    update readme and removed unnecessary xpath usage

--- igrp-aio.py
+++ igrp-aio.py
@@ -1,71 +1,95 @@
 '''
-O que o programa faz:
+[ICOMPLETE]
 
-    Iª PARTE:
-        *+ entrar no site da nosi;
-        *+ colocar o utilizador e a password;
-        *+ entrar no SIGE (Sistema Integrado de Gestao Escolar);
+What the script does:
 
-    IIª PARTE:
-        * fazer download da lista dos alunos por turma;
-        * mostrar output numa folha de calculos (se possível organizado e formatado)
+    I PART:
+    + sign in to Nosi's website;
+    + place the user and password;
+    + go to SIGE (Sistema Integrado de Gestao Escolar | Integrated School Management System);
 
-    IIIª PARTE:
-        *fazer upload com inputs de dados introduzidos na folha de cálculo baixado
+    II PART:
+        * download the list of students by class;
+        * show output on a spreadsheet (if possible organized and formatted);
+
+    III PART:
+        *upload with data inputs entered in the downloaded spreadsheet.
 '''
 import config
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup as bs
 from time import sleep
 
 
-# Iª PARTE
-browser = webdriver.Firefox()
+# I PART
+driver = webdriver.Firefox()
+driver.get('https://nosiapps.gov.cv/redglobal/redglobal.glb_dispatcher.login')
+
 
 def login():
 
-    browser.get('https://nosiapps.gov.cv/redglobal/redglobal.glb_dispatcher.login')
-    browser.implicitly_wait(20)
-
-    # fazer login
     for i in range (0,2):
 
-        wait_login = WebDriverWait(browser, 20)
+        try:
+            wait_login = WebDriverWait(driver, 20)
+            utilizador = wait_login.until(EC.element_to_be_clickable((By.NAME, 'p_login')))
+            driver.find_element_by_name('p_login').send_keys(config.username)
 
-        utilizador = wait_login.until(EC.element_to_be_clickable((By.NAME, 'p_login')))
-        browser.find_element_by_name('p_login').send_keys(config.username)
+            Keys.TAB
 
-        Keys.TAB
+            password = wait_login.until(EC.element_to_be_clickable((By.NAME, 'p_password')))
+            driver.find_element_by_name('p_password').send_keys(config.password)
 
-        password = wait_login.until(EC.element_to_be_clickable((By.NAME, 'p_password')))
-        browser.find_element_by_name('p_password').send_keys(config.password)
-        browser.find_element_by_name('p_button').click()
-        sleep(2)
+            button = wait_login.until(EC.element_to_be_clickable((By.NAME, 'p_button')))
+            sleep(1)
+            driver.find_element_by_name('p_button').click()
+
+            sleep(3)
+
+        except TimeoutException:
+            print("Timed out waiting for login page to load")
 
 def abrir_menu():
-    menu_lateral = esperar.until(EC.element_to_be_clickable((By.ID,'side-bar-ctrl')))
-    browser.find_element_by_id('side-bar-ctrl').click()#abrir o menu
 
-    menu_gestao_disciplina = esperar.until(EC.element_to_be_clickable((By.XPATH,'/html/body/form/div[1]/div/div[1]/ul/li[2]/a')))
-    browser.find_element_by_xpath('/html/body/form/div[1]/div/div[1]/ul/li[2]/a').click()#Menu gestao de disciplina vertical
-    browser.find_element_by_xpath('/html/body/form/div[1]/div/div[1]/ul/li[2]/ul/li[2]/a/span').click()# Menu principal do professor
+    try:
 
-    gestao_disciplina = esperar.until(EC.element_to_be_clickable((By.XPATH, '/html/body/form/div[1]/div/div[2]/div/div[2]/div/div/div/ul/li[3]/a/span')))
-    browser.find_element_by_xpath('/html/body/form/div[1]/div/div[2]/div/div[2]/div/div/div/ul/li[3]/a/span').click()# menu gestao_disciplina horizontal
+        #icone
+        wait_menu = WebDriverWait(driver,20)
+        icon = wait_menu.until(EC.element_to_be_clickable((By.CLASS_NAME,'app-icon')))
+        driver.find_element_by_class_name('app-icon').click()
 
-    browser.find_element_by_xpath('/html/body/form/div[1]/div/div[2]/div/div[2]/div/div/div/div/div[3]/div/div/div/div/div/a[3]/span').click()# atribuir notas
+        #abrir o menu
+        menu_lateral = wait_menu.until(EC.element_to_be_clickable((By.ID,'side-bar-ctrl')))
+        driver.find_element_by_id('side-bar-ctrl').click()
+
+        #Menu gestao de disciplina
+        menu_gestao_disciplina = wait_menu.until(EC.element_to_be_clickable((By.LINK_TEXT,'Gestão de Disciplina')))
+        driver.find_element_by_link_text('Gestão de Disciplina').click()
+
+        #Menu principal do professor
+        menu_p_professor = wait_menu.until(EC.element_to_be_clickable((By.LINK_TEXT,'Menu Principal Professor')))
+        driver.find_element_by_link_text('Menu Principal Professor').click()
+
+        gestao_disciplina = esperar.until(EC.element_to_be_clickable((By.XPATH, '/html/body/form/div[1]/div/div[2]/div/div[2]/div/div/div/ul/li[3]/a/span')))
+        driver.find_element_by_xpath('/html/body/form/div[1]/div/div[2]/div/div[2]/div/div/div/ul/li[3]/a/span').click()# menu gestao_disciplina horizontal
+
+        driver.find_element_by_xpath('/html/body/form/div[1]/div/div[2]/div/div[2]/div/div/div/div/div[3]/div/div/div/div/div/a[3]/span').click()# atribuir notas
+
+    except TimeoutException:
+        print("Timed out waiting for summary_page to load")
 
 
 
 
-# IIª PARTE
+# II PART
 
 #filtro_nota = esperar.until(EC.element_to_be_clickable((By.XPATH, '/html/body/form/div[1]/div/div[2]/div/div[4]/div[2]/div/div/div[2]/div/div[3]/span/span[1]/span')))
-# browser.find_element_by_xpath('/html/body/form/div[1]/div/div[2]/div/div[4]/div[2]/div/div/div[2]/div/div[3]/span/span[1]/span').click()# atribuir notas filtro ano
+# driver.find_element_by_xpath('/html/body/form/div[1]/div/div[2]/div/div[4]/div[2]/div/div/div[2]/div/div[3]/span/span[1]/span').click()# atribuir notas filtro ano
 
 
 #Init the script
--- igrp-summary.py
+++ igrp-summary.py
@@ -15,26 +15,27 @@ from selenium.webdriver.common.by import By
 from time import sleep
 
 
-browser = webdriver.Firefox()
-browser.get('https://nosiapps.gov.cv/redglobal/redglobal.glb_dispatcher.login')
+driver = webdriver.Firefox()
+driver.get('https://nosiapps.gov.cv/redglobal/redglobal.glb_dispatcher.login')
+
 
 def login():
 
     for i in range (0,2):
 
         try:
-            wait_login = WebDriverWait(browser, 20)
+            wait_login = WebDriverWait(driver, 20)
             utilizador = wait_login.until(EC.element_to_be_clickable((By.NAME, 'p_login')))
-            browser.find_element_by_name('p_login').send_keys(config.username)
+            driver.find_element_by_name('p_login').send_keys(config.username)
 
             Keys.TAB
 
             password = wait_login.until(EC.element_to_be_clickable((By.NAME, 'p_password')))
-            browser.find_element_by_name('p_password').send_keys(config.password)
+            driver.find_element_by_name('p_password').send_keys(config.password)
 
             button = wait_login.until(EC.element_to_be_clickable((By.NAME, 'p_button')))
             sleep(1)
-            browser.find_element_by_name('p_button').click()
+            driver.find_element_by_name('p_button').click()
 
             sleep(3)
 
@@ -45,29 +46,29 @@ def summary_page():
 
     try:
         #icone
-        wait_menu = WebDriverWait(browser,20)
+        wait_menu = WebDriverWait(driver,20)
         icon = wait_menu.until(EC.element_to_be_clickable((By.CLASS_NAME,'app-icon')))
-        browser.find_element_by_class_name('app-icon').click()
+        driver.find_element_by_class_name('app-icon').click()
 
         #abrir o menu
         menu_lateral = wait_menu.until(EC.element_to_be_clickable((By.ID,'side-bar-ctrl')))
-        browser.find_element_by_id('side-bar-ctrl').click()
+        driver.find_element_by_id('side-bar-ctrl').click()
 
         #Menu gestao de disciplina
         menu_gestao_disciplina = wait_menu.until(EC.element_to_be_clickable((By.LINK_TEXT,'Gestão de Disciplina')))
-        browser.find_element_by_link_text('Gestão de Disciplina').click()
+        driver.find_element_by_link_text('Gestão de Disciplina').click()
 
         #Menu principal do professor
         menu_p_professor = wait_menu.until(EC.element_to_be_clickable((By.LINK_TEXT,'Menu Principal Professor')))
-        browser.find_element_by_link_text('Menu Principal Professor').click()
+        driver.find_element_by_link_text('Menu Principal Professor').click()
 
         #menu sala de aula
         sala_aula = wait_menu.until(EC.element_to_be_clickable((By.LINK_TEXT,'Sala Aula')))
-        browser.find_element_by_link_text('Sala Aula').click()
+        driver.find_element_by_link_text('Sala Aula').click()
 
         #submenu resumo do dia
         resumo_dia = wait_menu.until(EC.element_to_be_clickable((By.LINK_TEXT,'Resumo do dia')))
-        browser.find_element_by_link_text('Resumo do dia').click()
+        driver.find_element_by_link_text('Resumo do dia').click()
 
     except TimeoutException:
         print("Timed out waiting for summary_page to load")
