d15784ea2c30810f03b0c12069aa456284f1ebea
David Nunes <51808846+Dnuns@users.noreply.github.com>
2022-01-01 20:22:33 -0100

    add bat file for windows

--- igrp-script.py
+++ igrp-script.py
@@ -16,6 +16,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 from time import sleep
 
 class igrp_script:
---
+++ start_IGRP.bat
@@ -0,0 +1,4 @@
+@echo off
+title IGRP SCRIPT
+python C:\Users\davsn\Documents\projects\igrp-scripts\igrp-script.py
+pause
